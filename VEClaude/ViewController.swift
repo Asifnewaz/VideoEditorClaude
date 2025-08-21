722c2d090e40226442bb4d43e5d351ccffc8887b
Asif Newaz <asif.newaz@braincraftapp.com>
2025-08-21 21:08:01 +0200

    Initial Commit

---
+++ VEClaude/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VEClaude
+//
+//  Created by BCL Device 24 on 21/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
